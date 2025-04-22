2b8d85956c6268de1b7140efdd78dda10bf9df2b
Matias <lit.io30303@gmail.com>
2025-04-21 22:22:00 -0400

    fix bug

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,23 +14,23 @@ const Projects = () => {
       </motion.h2>
       <div>
         {PROJECTS.map((project, index) => (
-          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center justify-center ">
             <motion.div
               whileInView={{ opacity: 1, y: 0 }}
               initial={{ opacity: 0, y: -100 }}
               transition={{ duration: 1 }}
-              className="w-full lg:w-1/4"
+              className="w-full lg:w-1/4 flex justify-center"
             >
               <img
                 src={project.image}
                 alt={project.title}
                 width={250}
                 height={250}
-                className="mb-6 rounded"
+                className="mb-6 rounded mx-auto"
               />
             </motion.div>
             <motion.div
-              whileInView={{ opacity: 1, y: 0 }}
+              whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: 100 }}
               transition={{ duration: 1 }}
               className="w-full max-w-xl lg:w-3/4"
@@ -39,7 +39,7 @@ const Projects = () => {
               <p className="mb-4 text-stone-400">{project.description}</p>
               {project.technologies.map((tech, index) => (
                 <span
-                  className="mr-2 rounded bg-stone-900 p-2 text-sm font-medium text-stone-300"
+                  className="mr-2 rounded bg-stone-900 p-2 text-sm font-medium text-stone-300 "
                   key={index}
                 >
                   {tech}
